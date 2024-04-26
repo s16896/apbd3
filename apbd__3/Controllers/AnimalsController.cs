aca5673166ffe5a12e96124dd942aad174346684
s16896 <s16896@pjwstk.edu.pl>
2024-04-26 10:09:23 +0200

    APBD 3

---
+++ apbd__3/Controllers/AnimalsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using apbd__3.Models;
+using apbd__3.Services;
+
+namespace apbd__3.Controllers
+{
+    [Route("api/animals")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly IDBService DBService;
+
+        public AnimalsController(IDBService DBService)
+        {
+            this.DBService = DBService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAnimals([FromQuery] string orderBy)
+        {
+            List<Animal> animals = null;
+            animals = DBService.GetAnimals(orderBy);
+          
+            return Ok(animals);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAnimal([FromBody] Animal animal)
+        {
+            try { DBService.CreateAnimal(animal); }
+            catch (Exception) { return BadRequest("Entered data is not valid!"); }
+            return Ok("Succsesfully created!");
+        }
+
+        [HttpPut("{idAnimal}")]
+        public async Task<IActionResult> ChangeAnimal([FromRoute] int idAnimal, [FromBody] Animal animal)
+        {
+            DBService.ChangeAnimal(idAnimal, animal); 
+
+            return Ok("Succsesfully changed!");
+        }
+
+        [HttpDelete("{idAnimal}")]
+        public async Task<IActionResult> DeleteAnimal([FromRoute] int idAnimal)
+        {
+             DBService.DeleteAnimal(idAnimal); 
+        
+            return Ok("Succsesfully deleted!");
+        }
+
+
+    }
+}
---
+++ apbd__3/Services/IDBService.cs
@@ -0,0 +1,12 @@
+﻿using apbd__3.Models;
+
+namespace apbd__3.Services
+{
+    public interface IDBService
+    {
+        List<Animal> GetAnimals(string orderBy);
+        void CreateAnimal(Animal animal);
+        void ChangeAnimal(int idAnimal, Animal animal);
+        void DeleteAnimal(int idAnimal);
+    }
+}
